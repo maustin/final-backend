d109debd0187bebcae539d38dc1ff1df4aec16c1
Michael Austin <maustin@accesso.com>
2020-01-18 20:18:10 -0800

    PurchaseOrderItem complete

--- models/purchaseOrderItem/router.js
+++ models/purchaseOrderItem/router.js
@@ -1,1 +1,18 @@
-// This even necessary?
+let express = require('express');
+let model = require('./model');
+let router = express.Router();
+
+router.get('/:id', (request, response, next) => {
+	model.readOne(request.params.id, (error, data) => {
+		if (error) {
+			console.error('PurchaseOrderItem model readOne error:', error);
+			response.sendStatus(500);
+		}
+		else if (data)
+			response.json(data);
+		else
+			response.status(404).send('Cannot find purchase order item matching criteria.');
+	});
+});
+
+module.exports = router;
